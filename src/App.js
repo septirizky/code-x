124d4ecb3f69e95658ceda035071cd05da339e33
Septi Rizki <septirizky79@gmail.com>
2023-10-23 15:34:12 +0700

    add client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "./App.css";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Login } from "./components/Login";
+import Home from "./components/Home";
+import { Users } from "./components/Users";
+import Posting from "./components/Posting";
 
 function App() {
+  // const setToken = (data) => {
+  //   sessionStorage.setItem("token", JSON.stringify(data.token));
+  //   sessionStorage.setItem("username", JSON.stringify(data.username));
+  //   sessionStorage.setItem("image", JSON.stringify(data.image));
+  //   sessionStorage.setItem("iduser", JSON.stringify(data.iduser));
+  //   sessionStorage.setItem("userdata", JSON.stringify(data.data));
+  // };
+  // const getToken = () => {
+  //   const tokenString = sessionStorage.getItem("token");
+  //   const userToken = JSON.parse(tokenString);
+  //   return userToken?.toString();
+  // };
+  // const token = getToken();
+  // if (!token) {
+  //   return <Login setToken={setToken} />;
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />}>
+            <Route index element={<Home />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/items" element={<Posting />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
